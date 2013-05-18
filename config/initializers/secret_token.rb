da9ac7a87b88345d5c33009af3754eee0d1a9fb8
chihungyu1116 <chihungyu1116@gmail.com>
2013-05-18 12:06:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gulp::Application.config.secret_token = '413ccaf0261ff93473d18195178b3461099f10734062a9c027cc79049354c7403efc3a4f2b56e493fc4bda26e162e27b7e2e3d5a51669ca6825682e3f3321464'
